016604e664df86206cb3eb2ef0913f1a6827569a
azucenareyes <sofia878@gmail.com>
2020-07-26 14:56:00 -0600

    Updates

---
+++ precache-manifest.cba5dda25830c0bc946f029e1f4f1031.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2983385a26b84cacbc7bb834d835da9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b25a3af088876a9b9d19",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "355442195355c0985cef",
+    "url": "/static/js/2.877693c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.877693c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25a3af088876a9b9d19",
+    "url": "/static/js/main.c24c2d90.chunk.js"
+  },
+  {
+    "revision": "c8f468104d0ff58a20a3",
+    "url": "/static/js/runtime-main.24dc260c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
